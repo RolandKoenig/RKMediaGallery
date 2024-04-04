5e93c61d9071838df6234285913d1fe18e19a67e
Roland König <roland.koenig@rolandk.de>
2024-04-04 20:40:07 +0200

    Added publish script for RKMediaGallery.PictureConvertUtility

--- scripts/publish-macos.ps1
+++ scripts/publish-macos.ps1
@@ -2,7 +2,12 @@
 Remove-Item ../publish -Recurse -Force
 mkdir ../publish
 
-# Create macOS app package
+# Create macOS app package (RK Media Gallery)
 dotnet publish -c Release -r osx-arm64 --self-contained -o ../publish/RKMediaGallery/Contents/MacOS -p:PublishReadyToRun=true ../src/RKMediaGallery/RKMediaGallery.csproj
-cp -r ../macos-app-template/* ../publish/RKMediaGallery/Contents
+cp -r ../macos-app-template/mediagallery/* ../publish/RKMediaGallery/Contents
 mv ../publish/RKMediaGallery ../publish/RKMediaGallery.app
+
+# Create macOS app package (RK Media Gallery)
+dotnet publish -c Release -r osx-arm64 --self-contained -o ../publish/RKMediaGalleryConvertUtility/Contents/MacOS -p:PublishReadyToRun=true ../src/RKMediaGallery.PictureConvertUtility/RKMediaGallery.PictureConvertUtility.csproj
+cp -r ../macos-app-template/mediagallery-convertutility/* ../publish/RKMediaGalleryConvertUtility/Contents
+mv ../publish/RKMediaGalleryConvertUtility ../publish/RKMediaGalleryConvertUtility.app
