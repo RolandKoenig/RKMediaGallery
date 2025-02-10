d33ad3becb418f9030149f90618f4ec4aced101f
Roland König <roland.koenig@rolandk.de>
2025-02-10 08:50:54 +0800

    Renamed to RolandK MediaGallery

--- scripts/publish-macos.ps1
+++ scripts/publish-macos.ps1
@@ -5,9 +5,9 @@ mkdir ../publish
 # Create macOS app package (RK Media Gallery)
 dotnet publish -c Release -r osx-arm64 --self-contained -o ../publish/RKMediaGallery/Contents/MacOS -p:PublishReadyToRun=true ../src/RKMediaGallery/RKMediaGallery.csproj
 cp -r ../macos-app-template/mediagallery/* ../publish/RKMediaGallery/Contents
-mv ../publish/RKMediaGallery ../publish/RKMediaGallery.app
+mv ../publish/RKMediaGallery "../publish/RolandK MediaGallery.app"
 
 # Create macOS app package (RK Media Gallery)
 dotnet publish -c Release -r osx-arm64 --self-contained -o ../publish/RKMediaGalleryConvertUtility/Contents/MacOS -p:PublishReadyToRun=true ../src/RKMediaGallery.PictureConvertUtility/RKMediaGallery.PictureConvertUtility.csproj
 cp -r ../macos-app-template/mediagallery-convertutility/* ../publish/RKMediaGalleryConvertUtility/Contents
-mv ../publish/RKMediaGalleryConvertUtility ../publish/RKMediaGalleryConvertUtility.app
+mv ../publish/RKMediaGalleryConvertUtility "../publish/RolandK MediaGallery ConvertUtility.app"
